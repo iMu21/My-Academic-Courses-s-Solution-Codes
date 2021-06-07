7076d40bea63490f67d401b40b9623c42363a188
Md Imran Khan <ik21.cse14@gmail.com>
2021-06-07 12:51:12 +0600

    Update Lab2.c

--- Compilers and Automata Theory Lab/Lab2.c
+++ Compilers and Automata Theory Lab/Lab2.c
@@ -7,25 +7,27 @@ int main()
     printf("\nThe string is ");
 
     int i,j,flag=0;
-    for(i=0;i<sizeof(str);i++){
-        if(str[i]=='a'){
-            printf("under the rule 'a'");
-            flag=1;
-        for(j=i+1;j<sizeof(str);j++){
-            if(str[j]=='b'&& str[j+1]>='a' && str[j+1]<='z'){
+
+    if(str[0]=='a')
+    {
+        printf("under the rule 'a'");
+        flag=1;
+        for(j=1; j<sizeof(str); j++)
+        {
+            if(str[j]=='b'&& str[j+1]>='a' && str[j+1]<='z')
+            {
                 printf(", 'a*b+'");
             }
         }
-        break;
-        }
-    }
-    for(i=0;i<sizeof(str)-2;i++){
-        if(str[i]=='a'&&str[i+1]=='b'&&str[i+2]=='b'){
-            printf(", 'abb'");
-        }
     }
 
-    if(flag==0){
+   if(strcmp(str,"abb")==0){
+    printf(", 'abb' ");
+   }
+
+
+    if(flag==0)
+    {
         printf("not under any rule");
     }
     printf(".\n");
